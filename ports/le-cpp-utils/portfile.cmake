432dab5ff74dd0276589c06d161d4ef9f2eb4af1
Jaakko Rantala <jaakko.rantala@gmail.com>
2025-05-16 17:30:31 +0300

    Fixed le-cpp-utils port

--- ports/le-cpp-utils/portfile.cmake
+++ ports/le-cpp-utils/portfile.cmake
@@ -4,17 +4,13 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO lateralengine/le-cpp-utils
     REF "${VERSION}"
-    SHA512 0  # This is a temporary value. We will modify this value in the next section.
+    SHA512 b5838d69361b13892eacc2d94983392cf37a58f5d3344c09a815007688c24e13df8d952579fb3c7465cd2dfc894bced2afc9d8f72d3e7698f615cad1332c7ee7
     HEAD_REF main
 )
 
-vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
-)
-
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
-
-vcpkg_cmake_config_fixup(PACKAGE_NAME "le-cpp-utils")
+# vcpkg_cmake_config_fixup()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
